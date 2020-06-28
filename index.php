2a76d99f558d4502da1be084795c155bd1080f75
villeroamiel <55043122+villeroamiel@users.noreply.github.com>
2020-06-28 09:53:49 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+
+	.header {
+	font-family: Tahoma, Geneva, sans-serif;
+	background:#eaeaea;
+	text-align: center;
+	color: #212121;
+	}
+	
+	.logo {
+	}
+	
+	
+	.content{
+	font-family: Tahoma, Geneva, sans-serif;
+	color: #383838;
+	margin-left: auto;
+	margin-right: auto;
+	background: #d0d0d0;
+	width: 80%;
+	padding:1em;
+	}
+	.hview{
+	text-align: justify;
+	background: #f8f8f8;
+	display:grid;
+	grid-template-columns: 1fr 1fr;
+	grid-column-gap:2em;
+	}
+
+	.data{
+	text-align: justify;
+	background: #f8f8f8;
+	display:grid;
+	grid-template-columns: 1fr 2fr;
+	grid-column-gap:2em;
+	}
+	
+	</style>
+</head>
+
+<body>
+<div class="header">
+	<div class="logo">
+		</br>
+		<img src="amav.png" width="150">
+	</div>
+	<h1>Amiel Villero</h1>
+	<p>web && mobile developer || graphic artist</p>
+
+
+
+</div>
+
+	
+<div class="content">
+	<p>Marasbaras, Tacloban City | 09202094870 | villeroamiel@gmail.com</p>
+	<div class="hview"><div><h2>PROFILE</h2>
+	<p>Experienced Freelance Programmer. I am willing, eager and able to learn new skills  quickly. I am commited to completing tasks with compliance to the specifications of my clients.</p></div>
+
+	<div><h2>SKILLS</h2>
+	<p>Web Development & Design</p>
+	<p>Multimedia/Video Editing</p>
+	<p>Database Management/Design</p>
+	<p>Mobile App Development</p>
+	</div>
+	</div>
+	<h2>WORK EXPERIENCE</h2>
+	<div class="data">
+		
+		<div>
+			<h3>May2014 to October2014</h3>
+		</div>
+		<div>
+			<h3>Website Administrator</h3>
+			<h4>EscapeFan</h4>
+				<p>Writing content for games and websites.</p>
+				<p>Editing/creating Images for games.</p>
+				<p>Maintaining website traffic through social media.</p>
+		</div>
+<div>
+			<h3>March2013 to March2014</h3>
+		</div>
+		<div>
+			<h3>Web Security Consultant</h3>
+			<h4>Komodia</h4>
+				<p>Organizing datasheets.</p>
+				<p>Maintaining database.</p>
+				<p>Conducting website research.</p>
+		</div>
+</div>	
+	<h2>EDUCATION</h2>
+	<div class="data">
+		
+		<div>
+			<h3>2016</h3>
+		</div>
+		<div>
+			<h3>ACLC College Tacloban</h3>
+			<p>3rd year college level in BSIT</p>
+		</div>
+	</div>
+</div>
+	<div id="contact">
+Contact Us
+<form method="post" name="contact_form"
+action="contact-form-handler.php">
+    Your Name:
+    <input type="text" name="name">
+
+    Email Address:
+    <input type="text" name="email">
+
+    Message:
+    <textarea name="message"></textarea>
+
+    <input type="submit" value="Submit">
+</form>	
+
+</div>
+
+</body>
+</html>
